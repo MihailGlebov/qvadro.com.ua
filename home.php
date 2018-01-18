ea81c8c8c4654d80b31df1a32694daae77d653b9
MihailGlebov <35371455+MihailGlebov@users.noreply.github.com>
2018-01-18 21:50:14 +0200

    18/01/18
    11

---
+++ home.php
@@ -0,0 +1,228 @@
+<? 
+   require_once("/home/qvcom10/inc/msql.class");
+  $my=new class_msql;
+  $my->sql_connect();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<meta name="robots" content="index, follow" />
+<meta name="keywords" content="уличный терминал, термопринтер, купюроприемник, платежные терминалы, разработки, производство, терминалы оплаты, автоматы самообслуживания, продажа терминалов, терминал пополнения счета, оплата мобильной связи, круглосуточные терминалы, пополнение мобильного телефона, сеть терминалов, моментальные платежи, оплата услуг связи, продажа терминалов, быстрые on-line платежи, Харьков" />
+<meta name="description" content="Главные направления деятельности — уличные платежные терминалы самообслуживания, комплектующие к платежным терминалам, on-line сервис проведения платежей, платежная система Квадро, купюроприемники, принтеры, мониторы, разработки и производство" />
+<link href="css/qvadro.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" media="all" type="text/css" href="css/menu_style.css" />
+<!--[if IE]>
+<link rel="stylesheet" media="all" type="text/css" href="css/ie.css" />
+<![endif]-->
+<script type="text/javascript" src="js/menu.js"></script><title>Qvadro - Бизнес под ключ | Уличные платежные терминалы самообслуживания- эффективное решение для Вашего бизнеса</title>
+</head>
+<body  bgcolor="#F0F0F0" onLoad="allClose()"  >
+<table width="1200" border="0" cellpadding="0" cellspacing="0" align="center">
+<?php require_once("top.php"); ?>
+  <tr>
+    <td width="20" align="left" valign="top" bgcolor="#FFFFFF" class="style1"><img src="images/1ugol.gif" width="20" height="20"></td>
+    <td width="151" align="left" valign="middle" bgcolor="#FFFFFF" class="style1">Каталог</td>
+    <td align="right" valign="top" bgcolor="#FFFFFF" class="style1"><img src="images/2ugol.gif" width="20" height="20"></td>
+    <td align="left" valign="bottom" bgcolor="#F0F0F0" class="style1">&nbsp;</td>
+    <td width="20" align="left" valign="top" bgcolor="#FFFFFF"><img src="images/1ugol.gif" width="20" height="20"></td>
+    <td width="302" height="20" colspan="2" align="left" valign="bottom" bgcolor="#FFFFFF" class="navgoriz1" >
+	 <a class="navgoriz" href="id">Главная</a>
+	 <img src="images/go.jpg" width="20" height="18" align="absmiddle">
+	  Бизнес под ключ    </td>
+    <td height="20" colspan="3" align="right" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+    <td height="20" colspan="3" align="right" valign="top" bgcolor="#FFFFFF"><img src="images/2ugol.gif" alt="s" width="20" height="20"></td>
+    <td rowspan="2" align="left" valign="top" bgcolor="#F0F0F0">
+ 
+   
+      <br></td>
+    <td width="36" rowspan="2" align="left" valign="top" bgcolor="#FFFFFF"><img src="images/1ugol.gif" width="20" height="20"><br>	</td>
+    <td width="136" align="center" valign="middle" bgcolor="#FFFFFF"><span class="style1">Авторизация</span>	</td>
+	<td width="29" height="20" rowspan="2" align="right" valign="top" bgcolor="#FFFFFF"><img src="images/2ugol.gif" alt="покупка уличных платежных терминалов" width="20" height="20"></td>
+  </tr>
+  
+  <tr>
+    <td height="10" align="left" valign="top" bgcolor="#FFFFFF"></td>
+    <td height="10" align="left" valign="top" bgcolor="#FFFFFF">
+	<hr size="1" color="#CCCCCC">	</td>
+    <td height="10" align="left" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+    <td height="10" align="left" valign="top" bgcolor="#F0F0F0">&nbsp;</td>
+    <td width="20" rowspan="9" align="left" valign="top" bgcolor="#FFFFFF">	</td>
+    <td colspan="7" rowspan="9" align="left" valign="top" bgcolor="#FFFFFF">
+	
+	<table width="743" border="0" align="left" cellpadding="0">
+	  <tr>
+    <td colspan="3"><hr size="1" color="#CCCCCC"></td>
+    </tr>
+  <tr>
+    <td colspan="3">
+  <div class="corners"><div><div><div>
+	    <div>
+		<span class="blktema">Платежные терминалы – рентабельный бизнес под ключ </span>
+			      <hr size="1" color="#FFFFFF">
+	      <p class="blktext"><strong> Наша компания предлагает уникальный шанс </strong>для тех, кто хочет создать<strong> рентабельный бизнес </strong>по созданию сети платежных терминалов при единоразовых  затратах и получения стабильной постоянной прибыли. <br>
+	        <br>
+	          <strong>Как показывает практика</strong> — покупка уличных Терминалов самообслуживания довольно выгодный и перспективный бизнес. Этот рынок еще не занят и его емкость просто удивительна. За год наши дилеры начали работать в четырех областных центрах и около десятка небольших городов Украины. И с каждым днем мы получаем все больше предложений о сотрудничестве.	          <strong>Сейчас каждый человек может приобрести такой терминал </strong>и в кратчайшие сроки начать свой собственный бизнес. При этом, исходя из ежемесячной статистики, Покупатель Терминалов амообслуживания получает <strong>прибыль до 9%</strong> от общей суммы принятых денежных средств от населения с помощью Терминалов самообслуживания. В таком бизнесе важно успеть занять место.<br><br> 
+	          <strong>За Терминалами самообслуживания будущее.</strong> Наша собственная платежная система позволяет подключить любое количество фирм или организаций, принимающих электронные платежи.</p>
+ 	      <hr size="1" color="#FFFFFF">
+	      <br>
+	      </div></div></div></div>
+      </div>	    </td>
+    </tr>
+				 <tr>
+    <td colspan="2">&nbsp;</td>
+   </tr>
+	 <tr>
+    <td colspan="2">
+  <div class="corners"><div><div><div>
+	    <div>
+<span class="blktema">Что такое терминал самообслуживания компании &quot;Квадро+&quot;?</span>
+			      <hr size="1" color="#FFFFFF">	
+<p class="blktext"><strong>Терминал самообслуживания - </strong>это автоматическое устройство, которое соединено со специализированным программным обеспечением через GSM сеть, и используется для оплаты услуг связи мобильных операторов в Украине, интернет провайдеров, Web money, др.<br>
+  <br> 
+    <strong>Установка терминалов</strong> – возможность установки на круглые опоры – столбы (остановки, переходы).<br><br> 
+    <strong>Автономная работа Терминала - </strong>может полноценно функционировать от встроенного аккумулятора  до 6 суток (в комплекте поставки входит 2 аккумулятора + зарядное устройство). </p>
+<p class="blktext"><strong>Работа терминала от городского освещения - </strong> подключив Терминал к городскому освещению в вечернее время будет осуществляться автоматическая подзарядка встроенного аккумулятора (приблизительное время включения городского освещения на опорах с 18-00ч. до 24-00ч.). </p>
+<hr size="1" color="#FFFFFF">
+	<br>
+	      </div></div></div></div>
+      </div>	    </td>
+    </tr>
+			 <tr>
+    <td colspan="2">&nbsp;</td>
+   </tr>
+	 <tr>
+    <td colspan="2">
+  <div class="corners"><div><div><div>
+	    <div>
+<span class="blktema">В чем привлекательность данной установки?</span>
+			      <hr size="1" color="#FFFFFF">	
+<p class="blktext">Столбы (опоры) зачастую  принадлежат двум организациям: трамвайно – троллейбусному управлению или Горсвету.  Достаточно  заключить договор с одной организацией, и вы получаете столько мест для установки,  сколько пожелаете.  Терминалы будут находиться в местах наибольшего скопления людей.<br><br> <strong>Преимущества установки: - </strong>отсутствие конкуренции, низкая арендная плата, высокая проходимость потенциальных клиентов, не обусловленная конкретным магазином (организацией), время приема платежей от населения 24 часа в сутки. </p>
+        <hr size="1" color="#FFFFFF">
+	<br>
+	      </div></div></div></div>
+      </div>	    </td>
+    </tr>
+		 <tr>
+    <td colspan="2">&nbsp;</td>
+   </tr>
+	 <tr>
+    <td colspan="2">
+  <div class="corners"><div><div><div>
+	    <div>
+<span class="blktema">Расчет прибыли:</span>
+			      <hr size="1" color="#FFFFFF">	
+<p class="blktext"><strong>Средний доход владельца терминала составляет до 9%</strong> от общего оборота принятых платежей. 
+  Предположим, что Ваш Терминал самообслуживания принял от населения платежей на 30 000грн./мес. = (1000грн./1день х 30дней).  В этом случае <strong>Ваша прибыль составит 2700грн.</strong> = (30 000грн./1мес. х 9%)  </p>
+        <hr size="1" color="#FFFFFF">
+	<br>
+	      </div></div></div></div>
+      </div>	    </td>
+    </tr>
+	 <tr>
+    <td colspan="2">&nbsp;</td>
+   </tr>
+	 <tr>
+    <td colspan="2">
+  <div class="corners"><div><div><div>
+	    <div>
+<span class="blktema">Что нужно для бизнеса?</span>
+			      <hr size="1" color="#FFFFFF">	
+<p class="blktext"><strong> Сделать несколько шагов:</strong></p> 
+<ul class="blktext1" type="1">
+  <li>Купить терминалы;</li>
+  <br>
+  
+  <li>Арендовать места для установки платежных терминалов;</li>
+  <br>
+  
+  <li>Иметь государственную регистрацию субъекта предпринимательской деятельности;</li>
+  <br>
+  
+  <li>Заключить договор с нашей компанией для возможности подключения Ваших терминалов к нашей платежной системе «Квадро+».</li>
+</ul>
+ </p>
+        <hr size="1" color="#FFFFFF">
+	<br>
+	      </div></div></div></div>
+      </div>	    </td>
+    </tr>
+ <tr>
+    <td colspan="2">&nbsp;</td>
+   </tr>
+	 <tr>
+    <td colspan="2">
+  <div class="corners"><div><div><div>
+	    <div>
+<span class="blktema">Платежная система «Квадро+»</span>
+			      <hr size="1" color="#FFFFFF">	
+<p class="blktext">
+<strong>Платежная система «Квадро+» </strong>– состоит из трех основных частей: программного обеспечения в  платежных терминалах, процессингового программного обеспечения на сервере и биллинга оператора мобильной связи или другой платежной организации. 
+<br><br><strong>Процессинговое ПО</strong> обращается к базе данных мобильного оператора либо к серверу, который предоставляет платежные услуги. И в том, и в другом случае, результат успешно проведенной операции отображается в виде печатного чека, который сохраняется у покупателя или плательщика. <br><br><strong>Система платежей «Квадро+»</strong> позволяет мгновенно  оплачивать услуги мобильной связи, осуществлять через Интернет денежные переводы и многое другое. На сайте пользователь имеет возможность в режиме реального времени мониторить состояние терминалов, просматривать данные о пополнении (количество купюр, сумма  в терминале), получать данные о заряде батареи, состояние связи с сервером  и т.п. </p>
+        <hr size="1" color="#FFFFFF">
+	<br>
+	      </div></div></div></div>
+      </div>	    </td>
+    </tr>
+
+</table>	</td>
+    <td width="11" rowspan="9" align="left" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+    <td width="136" align="center" valign="middle" bgcolor="#FFFFFF">
+	<hr size="1" color="#CCCCCC">	</td>
+  </tr>
+  
+  <tr>
+    <td rowspan="7" valign="top" bgcolor= #FFFFFF></td>
+<td rowspan="7" valign="top" bgcolor= #FFFFFF>
+<? require_once("vmnu.php");?></td>
+    <td rowspan="7" valign="top" bgcolor= #FFFFFF></td>
+    <td rowspan="7" valign="top" bgcolor= #F0F0F0></td>
+    <td width="17" rowspan="8" align="left" valign="top" bgcolor="#F0F0F0">&nbsp;</td>
+    
+	<td  colspan="3" align="center" valign="bottom" bgcolor="#FFFFFF">
+<? require_once("requir/lg.php");?></td>
+  </tr>
+  <tr>
+    <td colspan="3" align="center" valign="top" bgcolor="#FFFFFF"></td>
+  </tr>
+  
+  <tr>
+    <td height="10" colspan="2" align="left" valign="bottom" bgcolor="#FFFFFF">
+	<img src="images/4ugol.gif" width="20" height="20">	</td>
+    <td width="29" height="10" align="right" valign="bottom" bgcolor="#FFFFFF"><img src="images/3ugol.gif" width="20" height="20"></td>
+  </tr>
+  <tr>
+    <td height="5" colspan="3" align="left" valign="middle" bgcolor="#F0F0F0">&nbsp;</td>
+  </tr>
+  <tr>
+   <td height="5" rowspan="2" align="left" valign="top" bgcolor="#FFFFFF"><img src="images/1ugol.gif" width="20" height="20">    	</td>
+    <td height="5" align="center" valign="middle" bgcolor="#FFFFFF"><span class="style1">Новые новости </span>	</td>
+    <td width="29" height="5" rowspan="2" align="right" valign="top" bgcolor="#FFFFFF"><img src="images/2ugol.gif" width="20" height="20"></td>
+  </tr>
+  <tr>
+    <td height="3" align="left" valign="top" bgcolor="#FFFFFF">	</td>
+  </tr>
+  <tr>
+    <td colspan="3" rowspan="2" align="left" valign="top" bgcolor="#FFFFFF">
+<? require_once("news.php");?></td>
+  </tr>
+  <tr>
+    <td valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+    <td valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+    <td width="20" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+    <td valign="top" bgcolor="#F0F0F0">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FFFFFF"><img src="images/4ugol.gif" width="20" height="20"></td>
+    <td bgcolor="#FFFFFF">&nbsp;</td>
+    <td bgcolor="#F0F0F0"><img src="images/3ugol.gif" width="20" height="20"></td>
+    <td bgcolor="#F0F0F0">&nbsp;</td>
+    <td align="left" valign="bottom" bgcolor="#FFFFFF"><img src="images/4ugol.gif" width="20" height="20"></td>
+    <td colspan="8" align="right" valign="bottom" bgcolor="#FFFFFF"><img src="images/3ugol.gif" width="20" height="20"></td>
+    <td bgcolor="#F0F0F0">&nbsp;</td>
+    <td align="left" valign="top" bgcolor="#FFFFFF"><img src="images/4ugol.gif" width="20" height="20"></td>
+    <td align="center" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+    <td align="right" valign="top" bgcolor="#FFFFFF"><img src="images/3ugol.gif" width="20" height="20"></td>
+<?php require_once("foot.php"); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<meta name="google-site-verification" content="YMHd4EwyCqKz3bZdH3G9-6SiLUQKB2zIsYB252BguPg" />
+<meta name="keywords" content="Автомат продажи воды, продажа очищенной воды из автоматов, бизнес с быстрым оборотом, вендинг - бизнес, семейный бизнес, производители вендинг – машин, стабильный бизнес, уличный терминал, паркомат, автоматы самообслуживания, Харьков" />
+<meta name="description" content="Автомат продажи воды: для привозной и для очищенной. Встраиваемый разливочный модуль. Бизнес под ключ с автоматами продажи воды, окупаемость 8 мес.тм Вода в кармане" />
+<title>Автомат продажи воды, терминалы, паркоматы от производителя </title>
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+	<!--[if lt IE 7]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+        	<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+    </div>
+	<![endif]-->
+    <!--[if lt IE 9]>
+   		<script type="text/javascript" src="js/html5.js"></script>
+        <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+	<![endif]-->
+<link rel="stylesheet" type="text/css" href="paradigm-plugin/css/settings.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="css/content.css" media="screen" />		
+	
+   <script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/jquery.reveal.js"></script>
+	<script src="js/tabs.js" type="text/javascript"></script>
+	<script type="text/javascript" src="fancybox/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="fancybox/jquery.fancybox-1.2.1.pack.js"></script>
+   <!-- jQuery Paradigm Slider  -->
+	<script type="text/javascript" src="paradigm-plugin/js/jquery.cssAnimate.mini.js"></script>	
+	<script type="text/javascript" src="paradigm-plugin/js/jquery.waitforimages.js"></script>	
+	<script type="text/javascript" src="paradigm-plugin/js/jquery.touchwipe.min.js"></script>	
+    <script type="text/javascript" src="paradigm-plugin/js/jquery.themepunch.paradigm.js"></script>	
+	
+<script type="text/javascript">
+		$(document).ready(function() {
+			$.noConflict(); 
+		 					jQuery('#banner-example-1').paradigm( {										
+							width:800,
+							height:300,
+							
+							//thumbWidth:90,
+							//thumbHeight:50,
+							//thumbAmount:4,							
+							//thumbSpaces:4,
+							//thumbPadding:4,
+							
+							shadow:'true',
+							
+							//parallaxX:500,
+							//parallaxY:10,
+							//captionParallaxX:-40,
+							//captionParallaxY:2,
+							
+							touchenabled:'on',
+							
+							timer:5						
+						});
+		 
+		});
+</script>  	
+	
+	
+
+</head>
+
+<body id="page5">
+	<div class="bg">
+        <!--==============================header=================================-->
+       <header>
+ 				<? include_once("inc/top.php");?>					
+       </header>
+        <!--==============================content================================-->
+        <section id="content">
+            <div class="main">
+                <div class="container_12">
+                    <div class="wrapper">
+					   <article class="grid_4">
+							<? include_once("inc/leftmenu.php");?>
+                      </article>
+						
+                    	<article class="grid_9">	
+						
+<div id="banner-example-1" class="light" style="margin-top:0px; margin-left:0px;margin-bottom:30px; ">
+				<ul>								
+					<!-- THE 1. SLIDE -->
+					<li data-transition="fade"><img src="images/slides/slide1.jpg" data-thumb="images/thumbs/thumb1.jpg" data-thumb_bw="images/thumbs/thumb1_bw.jpg">
+																											
+							<div  class="creative_layer">
+	<div class="caption_blue fadeup" style="top:170px;left:50px;">Компания Квадро</div>																
+	<div class="caption_black smallcap faderight" style="top:213px;left:90px">
+	производство терминалов и паркоматов</div>										
+							</div>
+					</li>
+	<!-- THE 4. SLIDE -->
+					<li data-transition="fade"><img src="images/slides/slide4.jpg" >
+					<div  class="creative_layer">
+<div class="caption_blue1 fadeup" style="top:170px;left:80px;">Уличные терминалы и паркоматы</div>																
+		<div class="caption_white " style="top:210px;left:380px;">
+		<a href="business">подробнее о бизнесе...</a></div>										
+					  </div>
+					</li>
+					<!-- THE 6. SLIDE -->
+					<li data-transition="slide"><img src="images/slides/slide7.jpg" data-thumb="images/thumbs/thumb4.jpg" data-thumb_bw="images/thumbs/thumb4_bw.jpg">
+							<div  class="creative_layer">		
+	<div class="caption_blue fadeup" style="top:20px;right:50px;">Интернет-провайдерам</div>																
+	<div class="caption_black smallcap faderight" style="top:65px;right:90px">
+	установка уличных терминалов в пгт и селах</div>										
+							</div>
+					</li>				
+					<!-- THE 7. SLIDE -->
+					<li data-transition="fade"><img src="images/slides/slide11.jpg" data-thumb="images/thumbs/thumb5.jpg" data-thumb_bw="images/thumbs/thumb5_bw.jpg">
+																											
+	<div  class="creative_layer">
+	<div class="caption_blue fadeup" style="top:170px;right:50px;">Специализированные</div>																
+	<div class="caption_black smallcap faderight" style="top:213px;right:90px">
+	уличные паркоматы и терминалы</div>										
+	</div>
+					</li>
+				</ul>
+			</div>		
+<!-- THE END OF THE BANNER EXMAPLE -->
+   <article class="grid_61">
+               <h3 class="h3_info1">Уличные терминалы</h3>
+		    <p class="p0" align="center"> 
+			<div class="blok_images">
+			<a href="terminal" id="open_m0" data-animation="fade">
+			<img src="images/terminal/terminal.jpg"  >
+			</a>
+			</div>
+           <ul class="list-5">
+           	<li><a href="terminal">Терминал УПТ-1</a>
+			</li>
+            </ul>
+		</p>
+    </article>
+
+<article class="grid_61">
+        <h3 class="h3_info1">Терминалы для помещений</h3>
+		<p class="p0" align="center">
+		 <div class="blok_images">
+		<a  href="terminalsensor" id="open_m1" data-animation="fade" >
+		<img src="images/sensor/sensor1.jpg"  >
+		</a>
+		</div>
+		<ul class="list-5">
+		<li>
+		<a href="terminalsensor">Сенсорный терминал для работы внутри помещений</a></li>
+		</ul>
+		</p>
+</article>
+         </article>
+<div class="grid_92">	
+<article class="grid_61">
+		<h3 class="h3_info1">Уличные паркоматы</h3>
+		  <p class="p0" align="center"> 
+		  <div class="blok_images">
+			<a href="parkomat" id="open_m0" data-animation="fade">
+			<img  src="images/parkomat/parkomat1.jpg"  >
+			</a>
+			</div>
+           <ul class="list-5">
+           	<li><a href="parkomat">Паркомат УПА-1</a>
+			</li>
+            </ul>
+		</p>
+    </article>
+<article class="grid_61">
+		<h3 class="h3_info1">Автоматы продажи воды</h3>
+		<p class="p0" align="center" > 
+		<a href="vodomat" id="open_m1" data-animation="fade" >
+		<!-- <a href="parkomatvorota" id="open_m1" data-animation="fade" >  -->
+		<div class="blok_images">
+		<img  src="images/vodomat/3.jpg"  >
+		</div>
+		</a>
+		<ul class="list-5">
+		<li><a href="vodomat">АПВ 500 и АПВ-200 осмос</a></li>
+		<!--<li><a href="parkomatvorota">АПВ 500-ПВ и АПВ-2х400gpt-ОВ</a></li>  -->
+		</ul>
+		</p>
+</article>
+         </div>
+		 	</div>
+                </div>
+				<br>
+           </div>
+        </section>
+    </div>
+<? include_once("inc/foot.php");?>
+</body>
+</html>
